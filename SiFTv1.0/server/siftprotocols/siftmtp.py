e450fec2567377583e6561cea3522863380113ba
Nguyen Tran <nguyentran010403@gmail.com>
2024-05-07 19:55:28 +0200

    minor change

--- SiFTv1.0/server/siftprotocols/siftmtp.py
+++ SiFTv1.0/server/siftprotocols/siftmtp.py
@@ -128,9 +128,9 @@ class SiFT_MTP:
 			_epd, _mac, _etk = self.split_encrypted_parts(encrypted_payload)  # Split correctly based on your message format
 
 			# Decrypting the AES key
-			private_rsa_key = RSA.importKey(open('test_pubkey.pem').read())
+			private_rsa_key = RSA.importKey(open('test_keypair.pem').read(),passphrase='crysys')
 			rsa_cipher = PKCS1_OAEP.new(private_rsa_key)
-			_tk = rsa_cipher.decrypt(_etk,passkey='crysys')
+			_tk = rsa_cipher.decrypt(_etk)
 
 			# AES-GCM Decryption
 			aes_gcm = AES.new(_tk, AES.MODE_GCM, nonce=parsed_msg_hdr['nonce'])
